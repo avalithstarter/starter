7c66a107a759b1ec5d1f3d56f8bd56425edbaad7
unknown <starter@avalith.net>
2023-08-04 15:22:21 -0300

    Fixes: removed more unused content

--- src/utils/content/content-helpers.ts
+++ src/utils/content/content-helpers.ts
@@ -4,8 +4,6 @@ import { readdirSync, readFileSync } from "fs";
 import { slugToTitle } from "@/utils/content/slug-to-title";
 import { compareAsc, isBefore } from "date-fns";
 
-type ContentTypes = "docs" | "blog";
-
 /**
  * Next requires a json-serializable object to be returned from getStaticProps
  * So we have to cleanup the metadata that gets returned a bit
@@ -42,43 +40,6 @@ type ContentPathsResponse = Array<ContentPathsObject>;
  * @param locale
  * @param contentType
  */
-export async function getContentPaths(
-  locale: string,
-  contentType: ContentTypes
-): Promise<ContentPathsResponse> {
-  const files = readdirSync(join(process.cwd(), basePath, contentType, locale));
-  return (
-    files
-      .map((filePath) => {
-        // clean up markdown extension and replace index files with a non-index slug
-        const slug = filePath.replace(/\.md$/, "").replace(/\/?index$/, "");
-        const { meta, content } = loadFileWithMeta(
-          join(process.cwd(), basePath, contentType, locale, filePath)
-        );
-
-        return {
-          slug,
-          fullPath: join("/", contentType, slug),
-          meta,
-          title: meta?.title || slugToTitle(slug),
-          content,
-        };
-      })
-      // remove unpublished files
-      .filter(
-        (file) =>
-          file.meta?.published &&
-          isBefore(new Date(file.meta.published), new Date())
-      )
-      // sort by publish date
-      .sort((fileA, fileB) => {
-        return compareAsc(
-          new Date(fileA.meta.published),
-          new Date(fileB.meta.published)
-        );
-      })
-  );
-}
 
 type GetContentBySlugResponse = {
   slug: string;
@@ -88,38 +49,6 @@ type GetContentBySlugResponse = {
   };
   content: string;
 };
-
-/**
- * Gets the full content object for a given content type, locale and slug
- * @param slug
- * @param locale
- * @param contentType
- */
-export async function getContentBySlug(
-  slug: string,
-  { locale, contentType }: { locale: string; contentType: ContentTypes }
-): Promise<GetContentBySlugResponse> {
-  /**
-   * We've probably been given a url slug, but it could be a file path
-   * so we normalize it here.
-   */
-  const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(
-    process.cwd(),
-    basePath,
-    contentType,
-    locale,
-    `${realSlug}.md`
-  );
-  const { meta, content } = loadFileWithMeta(fullPath);
-
-  return {
-    slug,
-    meta,
-    content,
-    title: meta?.title || slugToTitle(slug),
-  };
-}
 
 export type GetDocsNavigationResponse = {
   rootPaths: Array<ContentPathsObject>;
@@ -133,23 +62,4 @@ export type GetDocsNavigationResponse = {
  * This one does not apply to blogs
  * @param locale
  */
-export async function getDocsNavigation(
-  locale: string
-): Promise<GetDocsNavigationResponse> {
-  const navigation = {
-    rootPaths: [],
-    categories: {},
-  };
 
-  const filePaths = await getContentPaths(locale, "docs");
-  filePaths.forEach((filePath) => {
-    if (filePath.meta?.category) {
-      navigation.categories[filePath.meta.category] ||= [];
-      navigation.categories[filePath.meta.category].push(filePath);
-    } else {
-      navigation.rootPaths.push(filePath);
-    }
-  });
-
-  return navigation;
-}
