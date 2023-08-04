ef463b3ea88055dbe0e143eed7db053c806a3744
unknown <starter@avalith.net>
2023-08-04 14:30:33 -0300

    Fix: window object is now checked if available

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,11 +25,14 @@ function MyApp({ Component, pageProps, router }: AppProps) {
     // our dropdowns are used for navigation a lot
     // they work off css focus states, so they don't get removed
     // on navigation transitions.  this is a hack to force them to
-    const element = window?.document?.activeElement as HTMLElement;
-    if (typeof element?.blur === "function") {
-      element.blur();
+    if (typeof window === "object") {
+      const element = window?.document?.activeElement as HTMLElement;
+      if (typeof element?.blur === "function") {
+        element.blur();
+      }
     }
   }, [router.asPath]);
+  
   return (
     <Theme dataTheme={theme}>
       <SessionContextProvider
