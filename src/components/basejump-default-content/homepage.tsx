2419cc6df7e11a6b9cbcfe699af900725b5bf263
avalithstarter <starter@avalith.net>
2023-08-16 10:52:41 -0300

    add: button on homepage

--- src/components/basejump-default-content/homepage.tsx
+++ src/components/basejump-default-content/homepage.tsx
@@ -3,8 +3,17 @@ import Logo from "@/components/basejump-default-content/logo";
 import ContentMeta from "@/components/content-pages/content-meta";
 import PricingSection from "../pricing/pricing";
 import GeneralContent from "./general-content";
+import { useRouter } from "next/router";
 
-const BasejumpHomepage = () => (
+const BasejumpHomepage = () => {
+
+  const router = useRouter()
+
+  const handleNavigation = () =>{
+    router.push('/signup')
+  }
+
+  return(
     <div className="max-w-screen-lg mx-auto bg-base-100">
       <ContentMeta
         title="YourBrand"
@@ -18,12 +27,14 @@ const BasejumpHomepage = () => (
             Dashboard template for future {' '}
             <span className="text-primary">E-commerce</span>
           </h2>
+          <button onClick={handleNavigation} className="btn btn-primary btn-wide" style={{position:'relative', top:70, left:410}}>Get started for free</button>
         </div>
         
         <GeneralContent />
         <PricingSection />
       </div>
     </div>
-);
+  )
+};
 
 export default BasejumpHomepage;
